21e3ce35fd36a092a5cdc0bee4dc9acf61988238
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-02-06 14:30:18 -0500

    API update
    updated API to have director name, year of birth and death and given an id.
    
    updated documentation to reflect.

--- index.js
+++ index.js
@@ -11,64 +11,118 @@ let movieList = [
     {
         title : "Avatar" ,
         genre : "Sci-Fi/Action",
-        release_year: "2009"
+        release_year: "2009",
+        director : "James Cameron",
+        director_birth : "8/16/1954",
+        director_death : "n/a",
+        id : "1"
     },
 
     {
         title : "Event Horizon" ,
         genre : "Horror/Sci-Fi",
-        release_year: "1997"
+        release_year: "1997",
+        director : "Paul W.S. Anderson",
+        director_birth : "3/4/1965",
+        director_death : "n/a",
+        id : "2"
     },
 
     {
         title : "Spaceballs" ,
         genre : "Comedy",
-        release_year: "1987"
+        release_year: "1987",
+        director : "Mel Brooks",
+        director_birth : "6/28/1926",
+        director_death : "n/a",
+        id : "3"
     },
 
     {
         title : "True Grit" ,
         genre : "Western",
-        release_year: "1969"
+        release_year: "1969",
+        director : "Henry Hathaway",
+        director_birth : "3/13/1898",
+        director_death : "2/11/1985",
+        id : "4"
     },
 
     {
-        title : "Monty Python's The Meaning of Life" ,
-        genre : "Comedy/Musical",
-        release_year: "1983"
+        title : "Mortal Kombat" ,
+        genre : "Action/Fantasy",
+        release_year: "1995",
+        director : "Paul W.S. Anderson",
+        director_birth : "3/4/1965",
+        director_death : "n/a",
+        id : "5"
     },
 
     {
         title : "Resident Evil" ,
         genre : "Action/Horror",
-        release_year: "2002"
+        release_year: "2002",
+        director : "Paul W.S. Anderson",
+        director_birth : "3/4/1965",
+        director_death : "n/a",
+        id : "6"
     },
 
     {
         title : "Young Frankenstein" ,
         genre : "Horror/Comedy",
-        release_year: "1974"
+        release_year: "1974",
+        director : "Mel Brooks",
+        director_birth : "6/28/1926",
+        director_death : "n/a",
+        id : "7"
     },
 
     {
         title : "Lord of the Rings: The Fellowship of the Ring" ,
         genre : "Action/Fantasy",
-        release_year: "2001"
+        release_year: "2001",
+        director : "Peter Jackson",
+        director_birth : "10/31/1961",
+        director_death : "n/a",
+        id : "8"
     },
 
     {
         title : "The Princess Bride" ,
         genre : "Romance/Adventure",
-        release_year: "1987"
+        release_year: "1987",
+        director : "Rob Reiner",
+        director_birth : "3/6/1947",
+        director_death : "n/a",
+        id : "9"
     },
 
     {
         title : "Warcraft" ,
         genre : "Action/Fantasy",
-        release_year: "2016"
+        release_year: "2016",
+        director : "Ducan Jones",
+        director_birth : "5/30/1971",
+        director_death : "n/a",
+        id : "10"
     }
 ];
     
+
+let userList = [
+    {
+        id : "1",
+        name : "josh",
+        username : "jwass01"
+    },
+    {
+        id : "2",
+        name : "fred",
+        username : "freddo02"
+    }
+]
+
 
 app.use(morgan('common',));
 
@@ -83,10 +137,17 @@ app.get('/', (req, res) => {
 });
 
 //GET request to pull up user profile
-app.get('/users', (req, res) => {
-    res.send('User information works!')
+app.get('/users/:username', (req, res) => {
+    res.json(userList.find((user) =>
+    { return user.username === req.params.username }));
+});
     
-})
+
+//GET request to search a specific movie
+app.get('/movies/:title', (req, res) => {
+    res.json(movieList.find((movie) =>
+    { return movie.title === req.params.title }));
+});
 
 app.use (express.static('public'));
 
