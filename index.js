990e914afebf66a70e86b10a8c14d870f615d49a
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-02-28 10:34:04 -0500

    Troubleshooting env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express'),
     const Users = Models.User;
     const { check, validationResult } = require('express-validator');
 
-    mongoose.connect('mongodb+srv://jwasson:9wOlIaxMlxhpfd1R@cluster0.jzjsh.mongodb.net/Cluster0?retryWrites=true&w=majority',{
+    mongoose.connect('process.env.CONNECTION_URI',{
     useNewUrlParser: true, 
     useUnifiedTopology: true
 });
