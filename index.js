b80a6a0505415ca86121eef48ae030b1fef0bc19
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-03-20 17:27:37 -0400

    CORS change
    Added and updated CORS

--- index.js
+++ index.js
@@ -20,8 +20,19 @@ const express = require('express'),
 const passport = require('passport');
 // require('./passport');
 
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+
 const cors = require('cors');
-app.use(cors())
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) {// If a specific origin isn’t found on the list of allowed origins
+            let message = "The CORS policy for this application doesn't allow access from origin" + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
