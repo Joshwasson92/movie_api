af345400ad3f8bd0b2a44ba2c9dee874ec5b1d75
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-03-20 20:35:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -279,16 +279,19 @@ app.delete('/usersdelete/:Username', passport.authenticate('jwt', { session: fal
 //       });
 //   });
 
-  app.get("/movies", function (req, res) {
+  app.get('/movies', function (req, res) {
     Movies.find()
       .then(function (movies) {
         res.status(201).json(movies);
       })
       .catch(function (error) {
         console.error(error);
-        res.status(500).send("Error: " + error);
+        res.status(500).send('Error: ' + error);
       });
   });
+
+  
+
   
   app.use (express.static('public'));
   app.use((err, req, res, next) => {
