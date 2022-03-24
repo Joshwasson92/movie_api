1d4731dde80ada25aea79e26753989c0e980f22f
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-03-24 18:44:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,24 +17,27 @@ const express = require('express'),
         useUnifiedTopology: true 
     });
 
+    const cors = require('cors');
+    // app.use(cors({
+    //     origin: (origin, callback) => {
+    //         if (!origin) return callback(null, true);
+    //         if(allowedOrigins.indexOf(origin) === -1) {// If a specific origin isn’t found on the list of allowed origins
+    //             let message = "The CORS policy for this application doesn't allow access from origin" + origin;
+    //             return callback(new Error(message ), false);
+    //         }
+    //         return callback(null, true);
+    //     }
+    // }));
+    
+    app.use(cors())
+
+
 const passport = require('passport');
 // require('./passport');
 
 let allowedOrigins = ['http://localhost:8080', 'https://jwmovieapi.herokuapp.com/' , 'http://localhost:1234'];
 
-const cors = require('cors');
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if(allowedOrigins.indexOf(origin) === -1) {// If a specific origin isn’t found on the list of allowed origins
-//             let message = "The CORS policy for this application doesn't allow access from origin" + origin;
-//             return callback(new Error(message ), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
 
-app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
